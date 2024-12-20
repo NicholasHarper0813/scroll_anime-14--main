2b8466a4e0b93ed0f3cb29ca8bb381595cdf9101
nicholas harper <nicholasharper000@gmail.com>
2024-12-19 19:28:23 -0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
-function Hero() {
+function Hero() 
+{
   return (
     <header className="bg-hero bg-center bg-cover bg-no-repeat sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
       <div className="flex-1 flex flex-col gap-10">
