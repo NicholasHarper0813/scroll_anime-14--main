34bc99dbb0c029c572d9564811466a7e656d2eb9
nicholas harper <nicholasharper000@gmail.com>
2024-12-19 19:27:45 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { fetchAnime } from "./action";
 import LoadMore from "../components/LoadMore";
+import { fetchAnime } from "./action";
 
-async function Home() {
+async function Home() 
+{
   const data = await fetchAnime(1);
-
   return (
     <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
       <h2 className="text-3xl text-white font-bold">Explore Anime</h2>
