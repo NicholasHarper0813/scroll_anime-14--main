625d439bc57a58682224f0f02187ae25518cd5f8
nicholas harper <nicholasharper000@gmail.com>
2024-12-19 19:28:11 -0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
-function Footer() {
+function Footer() 
+{
   return (
     <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
       <p className="text-base font-bold text-white">@2023 EpicAnimeVault</p>
